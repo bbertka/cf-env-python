e8ba08f400907fee1308fcc6c1c967152215ead5
bbertka <bbertka@gmail.com>
2015-07-23 15:14:54 -0700

    update

--- cfenv.py
+++ cfenv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-from flask import Flask, request, redirect, url_for, send_from_directory
-import os
+from flask import Flask, request
+import os, json
 
 app = Flask(__name__)
 
@@ -9,7 +9,11 @@ port = int(os.getenv("VCAP_APP_PORT"))
 
 @app.route('/')
 def home():
-	return port
+	port = os.getenv("VCAP_APP_PORT")
+	apps = json.loads(os.getenv("VCAP_APPLICATION"))
+	serv = os.getenv("VCAP_SERVICES")
+	return json.dumps({'VCAP_APP_PORT': port, 
+		'VCAP_APPLICATION':apps, 'VCAP_SERVICES':serv })
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=port)
